d5e58c0fad00622e6b438939cc50476b1f1f2563
BekoSm <b.smagulov.1999@gmail.com>
2022-07-15 15:06:13 +0600

    Push#1

--- src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDetermainer.java
+++ src/main/java/school/mjc/stage0/conditions/task1/PositiveNumberDetermainer.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class PositiveNumberDetermainer {
     public void isPositive(int numberToBeDetermined) {
+        if (0<numberToBeDetermined){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
     }
 }
